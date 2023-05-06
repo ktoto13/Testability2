172d0a6f6dea8a759ce48390b865941c00c48359
Natis <traffkina@mail,ru>
2023-05-06 12:09:33 +0300

    change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+    public int calculate(double kgWeight, double mHeight) {
+        double result = kgWeight / (mHeight * mHeight);
+        return (int) result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BmiService service = new BmiService();
+        double kgWeight = 80.5;
+        double mHeight = 1.60;
+        int bmi = service.calculate(kgWeight, mHeight);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(bmi);
     }
 }
